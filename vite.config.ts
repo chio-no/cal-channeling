1f92698c4a8869960cbab45cefb16c71787f7ebf
chio-no <spz26tt9@gmail.com>
2025-09-16 11:24:06 +0900

    開発サーバをhttpsで立ち上げるように変更
    github pagesの前にvite.config.tsのpluginsをreactに戻すこと

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import basicCsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [basicCsl()],
   base: "./",
 });

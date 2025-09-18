7d03d09fa3c1a91c6778f105f5950438abde1d44
chio-no <spz26tt9@gmail.com>
2025-09-18 11:03:00 +0900

    デバッグ用の調整

--- src/services/googlePlaces.ts
+++ src/services/googlePlaces.ts
@@ -62,5 +62,7 @@ export async function searchNearbyFood(params: NearbyParams): Promise<Place[]> {
 
   const data = await res.json();
   const places: Place[] = data?.places ?? [];
+  console.log("searchNearbyFood", places);
+
   return places;
 }
